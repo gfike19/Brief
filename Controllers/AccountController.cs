459cd146838978379050435ca98f6078e504df34
Daniel Roberts <daniel.prescott.roberts@gmail.com>
2020-11-24 13:43:07 -0500

    add AccountController.cs

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Brief.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}

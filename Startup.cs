bcf18f41e74da3b644935fe55449e6d0ef7d6802
gfike19 <gfike19@gmail.com>
2021-01-21 17:24:19 -0600

    updating to use in dev ops starter

--- Startup.cs
+++ Startup.cs
@@ -100,7 +100,7 @@ namespace Brief
             endpoints.MapRazorPages();
             });
 
-            CreateUserRoles(services).Wait();
+            //CreateUserRoles(services).Wait();
         }
 
         private async Task CreateUserRoles(IServiceProvider serviceProvider)
